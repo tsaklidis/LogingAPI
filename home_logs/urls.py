9bbcbe237a527b72b82b1de1bc823743246dcc10
Stefanos Tsaklidis <stefanos@tsaklidis.gr>
2021-10-09 15:06:13 +0300

    Configure urls for GR|US

--- home_logs/urls.py
+++ home_logs/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url('^', include('home_logs.public.urls', 'public')),
     url('panel/', admin.site.urls),
+    url('^', include('home_logs.public.urls', 'public')),
     url('user/', include('home_logs.accounts.urls', 'user')),
     url('api/', include('home_logs.api.urls')),
 ]
