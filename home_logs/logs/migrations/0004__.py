993c1edfa659a7f26eb9830c2e0facd4740b21f1
Stefanos Tsaklidis <stefanos@tsaklidis.gr>
2021-10-09 11:19:41 +0300

    revert requirement

--- home_logs/logs/migrations/0004__.py
+++ home_logs/logs/migrations/0004__.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('logs', '0003_auto_20190920_1726'),
+        ('logs', '0003__'),
     ]
 
     operations = [
--- home_logs/property/migrations/0005__.py
+++ home_logs/property/migrations/0005__.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('property', '0004_auto_20191005_1935'),
+        ('property', '0004__'),
     ]
 
     operations = [
